5c4795ee98a6e83959489c0f4c2e4f94527ec91c
saleh-buamud <bbuamud@gmail.com>
2024-12-30 19:29:00 +1400

    Add images

--- resources/views/front-ecom-temp/Start-Call-Action.blade.php
+++ resources/views/front-ecom-temp/Start-Call-Action.blade.php
@@ -1,19 +1,19 @@
-   <section class="call-action section mt-4">
-       <div class="container">
-           <div class="row ">
-               <div class="col-lg-8 offset-lg-2 col-12">
-                   <div class="inner">
-                       <div class="content">
-                           <h2 class="wow fadeInUp" data-wow-delay=".4s">Currently You are using free<br>
-                               Lite version of ShopGrids</h2>
-                           <p class="wow fadeInUp" data-wow-delay=".6s">Please, purchase full version of the template
-                               to get all pages,<br> features and commercial license.</p>
-                           <div class="button wow fadeInUp" data-wow-delay=".8s">
-                               <a href="javascript:void(0)" class="btn">Purchase Now</a>
-                           </div>
-                       </div>
-                   </div>
-               </div>
-           </div>
-       </div>
-   </section>
+<section class="call-action section mt-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2 col-12">
+                <div class="inner">
+                    <div class="content">
+                        <h2 class="wow fadeInUp" data-wow-delay=".4s">حاليًا، أنت تستخدم النسخة المجانية<br>
+                            Lite من ShopGrids</h2>
+                        <p class="wow fadeInUp" data-wow-delay=".6s">يرجى شراء النسخة الكاملة من القالب
+                            للحصول على جميع الصفحات،<br> الميزات، والترخيص التجاري.</p>
+                        <div class="button wow fadeInUp" data-wow-delay=".8s">
+                            <a href="javascript:void(0)" class="btn">اشترِ الآن</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
