fccc196230f99fa3b268c40fb34f44abbd15f183
saleh-buamud <bbuamud@gmail.com>
2025-01-01 23:18:27 +1400

    Update message cart

--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -202,7 +202,7 @@
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="#">المخزون القطع</a></li>
-                                <li class="breadcrumb-item"><a href="{{ route('dashboard.allAdmin') }}">Admains</a>
+                                <li class="breadcrumb-item"><a href="{{ route('dashboard.allAdmin') }}">الادمن</a>
                                 </li>
 
                             </ol>
